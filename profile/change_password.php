0aee6cce4690d0c3446c60a7395991af8130cede
MMorshedAR <mahabubmorshed.ar@gmail.com>
2026-09-01 23:06:28 +0600

    Mahabub

---
+++ profile/change_password.php
@@ -0,0 +1,67 @@
+<?php
+
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+
+<title>Change Password</title>
+
+<link rel="stylesheet" href="../assets/css/style.css">
+
+</head>
+
+
+<body>
+
+
+<h2>Change Password</h2>
+
+
+
+<form action="change_password_process.php" method="POST">
+
+
+<label>Old Password:</label>
+
+<input type="password" name="old_password">
+
+
+
+<label>New Password:</label>
+
+<input type="password" name="new_password">
+
+
+
+<input type="submit" value="Change Password">
+
+
+<label>Confirm Password:</label>
+
+<input type="password" name="confirm_password">
+
+
+</form>
+<br><br>
+
+<center>
+
+<a class="back-btn" href="view_profile.php">
+
+Back To Profile
+
+</a>
+
+</center>
+
+</body>
+
+</html>
