1964369b0001cd67a59942164dff7c10fe4444fc
shoham7 <sahashoham2002@gmail.com>
2026-09-01 12:15:50 +0600

    shoham

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Courier Management System</title>
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+
+<body>
+
+
+<h2>Welcome To Courier Management System</h2>
+
+
+<div class="dashboard">
+
+
+<h3>Select User Type</h3>
+
+
+<a class="back-btn" href="customer/login.php">
+
+Customer
+
+</a>
+
+
+<br><br>
+
+
+<a class="back-btn" href="deliveryman/login.php">
+
+Delivery Man
+
+</a>
+
+
+<br><br>
+
+
+<a class="back-btn" href="admin/login.php">
+
+Admin
+
+</a>
+
+
+</div>
+
+
+
+</body>
+
+</html>
